4fc4da775c47de6e997ef5d85aef8e6f1a8c6fa1
Lucas Racoci <racoci.0@gmail.com>
2020-04-01 00:08:48 -0300

    Changing Information Maessage

--- src/extension.ts
+++ src/extension.ts
@@ -15,13 +15,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.visualText', () => {
 		// The code you place here will be executed every time your command is executed
-
+		
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Visual Text');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
